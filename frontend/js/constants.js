dce27845d0f790a4b24d60bc3074e3cf739cd082
papapapapython <60787935+papapapapython@users.noreply.github.com>
2022-07-07 13:55:23 +0300

    Update constants.js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Happy Flamingos Holders Club NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Happy Flamingos Holders Club NFT Project is a 5000 hand-drawn NFT Collection. Our main goal is to use web3 in order to create a community to share ideas inside the metaverse. Our early supporters will also have unique benefits to our future HF coin and game."  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
